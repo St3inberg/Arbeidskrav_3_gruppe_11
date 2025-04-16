f7a16f52f918041648684ad9933f8af0978076a4
Stephen Stenberg <stephen.stenberg@gmail.com>
2025-04-16 11:55:37 +0200

    work in progess

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,25 +1,25 @@
+// src/components/Nav.jsx
+
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { client } from '../sanityClient';
-import './Nav.css';
+import { client } from '../sanityClient'; 
 
 const Nav = () => {
   const [profiles, setProfiles] = useState([]);
 
   useEffect(() => {
-    
     client
       .fetch(`*[_type == "personer"]{ personname, productslug }`)
-      .then(data => setProfiles(data))
-      .catch(console.error);
+      .then((data) => setProfiles(data))
+      .catch((error) => console.error('Error fetching profiles:', error));
   }, []);
 
   return (
-    <nav className="navBar">
-      <ul className="navList">
-        {profiles.map(profile => (
-          <li key={profile.productslug.current} className="navItem">
-            <Link className="navLink" to={`/profile/${profile.productslug.current}`}>
+    <nav>
+      <ul>
+        {profiles.map((profile) => (
+          <li key={profile.productslug.current}>
+            <Link to={`/profile/${profile.productslug.current}`}>
               {profile.personname}
             </Link>
           </li>
