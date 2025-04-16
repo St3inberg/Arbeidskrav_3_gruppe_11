7eb4effb2ea5df478e06e6dd0c25d86aea208b5f
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-16 11:56:17 +0200

    laget komponelter
    jeg har starter med Router og satt inn komponelter som layout, Home og profilerkort

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Rollup } from 'vite'
+import { Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,13 @@ function App() {
   return (
     <>
       
+      <Layout>
+    
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/:slug' element={<profiler/>}></Route>
+      </Routes>
+      </Layout>
     </>
   )
 }
