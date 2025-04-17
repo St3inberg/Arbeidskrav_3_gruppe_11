187fafa3199c46a3d8c36c0b127184558a1a5166
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-17 16:18:55 +0200

    tar på HTML på profilkort-siden

---
+++ src/components/Arbidslogg.jsx
@@ -0,0 +1,10 @@
+export default function Arbidslogg(){
+    return(
+        <>
+        <h2>Arbeidslogg</h2>
+        <ul>
+            <li></li>
+        </ul>
+        </>
+    )
+}
--- src/components/profilkort.jsx
+++ src/components/profilkort.jsx
@@ -1,8 +1,22 @@
+
+
 export default function profilkort()
 {
 return(
     <>
-    
+    <article id={profil}>
+    <img src="#"></img>
+    <section>
+    <h2>navaene etternav</h2>
+    <p>
+        det som ståer i Bion
+    </p>
+    <h3>Interesser</h3>
+    <ul>
+        <li>1</li>
+    </ul>
+    </section>
+    </article>
     
     </>
 )
