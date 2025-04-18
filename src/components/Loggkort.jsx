767f9e31888aef6094925d3bbc7f2ba7d7ccee58
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-18 18:51:41 +0200

    begynt på ArbedsLoggen
    begynt på ArbedsLoggen
    
    også endret språk

---
+++ src/components/Loggkort.jsx
@@ -0,0 +1,13 @@
+export default function Loggkort(){
+
+    return(
+        <article>
+            <p>2025-03-12</p>
+            <p>Ingrid Øvrelid</p>
+            <p>oppgavenavet</p>
+            <p>kommerre hva man haregjort</p>
+            <p>2t</p>
+
+        </article>
+    )
+}
