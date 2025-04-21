628c0f8bd16e78afd3666ba49a6b4a92a673bc61
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-21 13:20:09 +0200

    prøve mer på fetch

--- src/components/ArbidsLogg.jsx
+++ src/components/ArbidsLogg.jsx
@@ -1,17 +1,23 @@
 import { useEffect } from "react"
 import { client } from "./client";
+import { data } from "react-router-dom";
 
 export default function Arbidslogg(){
+    const [arbeidslogg, setarbeidslogg] = useState([]);
+    
     
     const getWorkLog = async () => {
     client.fetch(`*[_type=="loggforing"]{}`
-    );
+    )
+    .then((data)=>{})
+
     }
     
    
 
     useEffect(()=>{
         getWorkLog();
+        console.log("", )
     })
 
 
--- src/components/Loggkort.jsx
+++ src/components/Loggkort.jsx
@@ -1,13 +1,11 @@
 export default function Loggkort(){
 
     return(
-        <article>
+        <article id="loggkort">
             <p>2025-03-12</p>
             <p>Ingrid Øvrelid</p>
             <p>oppgavenavet</p>
             <p>kommerre hva man haregjort</p>
-            <p>2t</p>
-
         </article>
     )
 }
