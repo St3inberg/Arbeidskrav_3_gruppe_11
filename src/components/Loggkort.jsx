67e789f22cbe851b949e3883f683f4024ef957f4
Stephen Stenberg <104306935+St3inberg@users.noreply.github.com>
2025-04-22 11:20:20 +0200

    Update Loggkort.jsx

--- src/components/Loggkort.jsx
+++ src/components/Loggkort.jsx
@@ -1,19 +1,1 @@
-export default function Loggkort({loggForhome}){
 
-    return(
-
-        <article className="loggkort">
-            <p>{loggForhome.dato}</p>
-            <p>Ingrid Øvrelid</p>
-            <p>oppgavenavet</p>
-            <p>{loggForhome.kommertar}</p>
-        </article>
-    )
-}
-
-{/* <article id="loggkort">
-<p>2025-03-12</p>
-<p>Ingrid Øvrelid</p>
-<p>oppgavenavet</p>
-<p>kommerre hva man haregjort</p>
-</article> */}
