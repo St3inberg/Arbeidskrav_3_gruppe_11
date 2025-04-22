f3910c1e824aab1b9fd70a70a2441132523628f9
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-22 12:51:13 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 // src/components/Home.jsx
 import React, { useEffect, useState } from 'react';
 import Personer from "./profilkort";
-
+import Loggkort from './Loggkort';
 
 
 
@@ -16,6 +16,15 @@ export default function Home({personer}) {
   {personer?.map((person) => (
         <Personer person={person} key={person._id} />
       ))}
+
+<section>
+    <h2>Arbeidslogg</h2>
+          
+    {loggforing?.map((loggForhome) => (
+      <Loggkort loggForhome={loggForhome}/>
+    ))}
+
+  </section>
 
 
   
