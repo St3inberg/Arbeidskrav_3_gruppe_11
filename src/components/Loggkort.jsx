2e1067b5efb9771a9a150f49719540630af3162a
ingridc23 <143408615+ingridc23@users.noreply.github.com>
2025-04-22 12:35:38 +0200

    Update Loggkort.jsx

--- src/components/Loggkort.jsx
+++ src/components/Loggkort.jsx
@@ -1,1 +1,13 @@
+export default function Loggkort({loggForhome }){
 
+  
+
+    return(
+
+        <article className="loggkort" >
+            <p>{loggForhome.dato}</p>
+            <p>{loggForhome.personname}</p>
+            <p>{loggForhome.kommentar}</p>
+        </article>
+    )
+}
